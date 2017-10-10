ed9553ceaa30f88b9d8265f0999e193f0d4061b8
root <vitonaychev@gmail.com>
2017-10-10 18:25:46 +0300

    Added sums controller

---
+++ app/controllers/sums_controller.rb
@@ -0,0 +1,21 @@
+require 'csv'
+
+class SumsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    def index
+    end
+    
+    def create
+        csv_file = params[:csv_file]
+        csv_file_path = csv_file.path
+        
+        sum = 0
+
+        CSV.foreach(csv_file_path) do |row|
+            sum += row[2].to_f
+        end
+
+        puts ("%.2f" % sum)
+        render :plain => ("%.2f\n" % sum).to_s
+    end
+end
